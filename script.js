514ba94a0eaf00310f885d4b4438155374fa80e4
hajjajmoath <hajjajmoath@gmail.com>
2023-03-02 02:01:57 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+let result = 0;
+let x = 0;
+let y = 0;
+
+function add(x,y) {
+    result = x + y;
+    return result;
+}
+
+function subtract(x,y) {
+    result = x - y;
+    return result;
+}
+
+function multiply(x,y) {
+    result = x * y;
+    return result;
+}
+
+function divide(x,y) {
+    result = x / y;
+    return result;
+}
