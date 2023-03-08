8a9497ab1d6f29126a908492fecee79e7da5b49b
hajjajmoath <hajjajmoath@gmail.com>
2023-03-08 21:50:39 +0300

    Update UI

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-let result = 0;
-let x = 0;
-let y = 0;
+const number = document.getElementsByClassName('number');
+const operator = document.getElementsByClassName('operator');
 
-function add(x,y) {
-    result = x + y;
-    return result;
-}
-
-function subtract(x,y) {
-    result = x - y;
-    return result;
-}
-
-function multiply(x,y) {
-    result = x * y;
-    return result;
-}
-
-function divide(x,y) {
-    result = x / y;
-    return result;
-}
+number.style.color = 'red';
